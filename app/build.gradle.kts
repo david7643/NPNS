5c1aa4ef47ad7231ef592f69f4af3cfdbef12257
myeong21 <jos69744@gmail.com>
2026-05-25 19:27:27 +0900

    Test_jsm_UIUX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,32 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Fragment
+    implementation(libs.androidx.fragment)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // Lifecycle (ViewModel, LiveData)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+
+    // MPAndroidChart (수면 데이터 그래프)
+    implementation(libs.mpandroidchart)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
